6b49dec4d8fae11547016c23a908f1c0e149fa35
fergusgault <fergus@gault.com>
2025-02-11 14:44:16 +0000

    Added top down view conversion

--- src/processing/frame_processing.py
+++ src/processing/frame_processing.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import logging
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -39,3 +40,20 @@ def crop_to_middle(frame, crop_fraction=0.8):
 
     # Return the cropped frame
     return frame[start_y:end_y, start_x:end_x]
+
+
+def get_top_down_view(frame_1, frame_2, table_pts_cam1, table_pts_cam2):
+    # Compute homography matrices
+    output_size = (800, 400)
+    table_rect = np.float32([[0, 0], [output_size[0], 0], [0, output_size[1]], [output_size[0], output_size[1]]])
+
+    H1 = cv.getPerspectiveTransform(table_pts_cam1, table_rect)
+    H2 = cv.getPerspectiveTransform(table_pts_cam2, table_rect)
+
+    top_down_view1 = cv.warpPerspective(frame_1, H1, output_size)
+    top_down_view2 = cv.warpPerspective(frame_2, H2, output_size)
+
+    # Stack frames vertically
+    stitched_frame = np.vstack((top_down_view1, top_down_view2))
+
+    return stitched_frame
