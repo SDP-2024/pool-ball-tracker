95808abcb430cee6a7f8708875f3ba085abfb421
fergusgault <fergus@gault.com>
2025-02-11 14:43:56 +0000

    Added experimental function for adjusting colors

--- src/processing/camera_adjustments.py
+++ src/processing/camera_adjustments.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2 as cv
 import cv2 as cv
 import numpy as np
-
+from skimage import exposure
+from skimage.exposure import match_histograms
 
 def compute_homography(corners, output_size):
     """
@@ -53,3 +54,31 @@ def warp_and_stitch(frame_1, frame_2, homography_1, homography_2, output_size):
     stitched = cv.addWeighted(warped_1, 0.5, warped_2, 0.5, 0)
 
     return stitched
+
+def equalize_frame(frame):
+    # Convert from BGR to YCrCb color space
+    ycrcb = cv.cvtColor(frame, cv.COLOR_BGR2YCrCb)
+    
+    # Split the channels
+    y, cr, cb = cv.split(ycrcb)
+    
+    # Equalize the histogram of the Y channel (luminance)
+    y_eq = cv.equalizeHist(y)
+    
+    # Merge the channels back
+    ycrcb_eq = cv.merge((y_eq, cr, cb))
+    
+    # Convert back to BGR color space
+    frame_eq = cv.cvtColor(ycrcb_eq, cv.COLOR_YCrCb2BGR)
+    
+    return frame_eq
+
+
+def match_colors(ref_frame, to_match):
+    ref_frame = cv.cvtColor(ref_frame, cv.COLOR_BGR2RGB)
+    to_match = cv.cvtColor(to_match,cv.COLOR_BGR2RGB)
+    
+    matched_frame = match_histograms(to_match, ref_frame, channel_axis = -1)
+    matched_frame = cv.cvtColor((matched_frame * 255).astype(np.uint8), cv.COLOR_RGB2BGR)
+
+    return matched_frame
