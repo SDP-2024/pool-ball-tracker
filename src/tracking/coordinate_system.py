9912ef4d1daba4f1223ebc0f394734a87acb701a
Fergus Gault <fergus@gault.com>
2025-02-20 12:34:46 +0000

    Adjusted for model

--- src/tracking/coordinate_system.py
+++ src/tracking/coordinate_system.py
@@ -14,13 +14,20 @@ class Coordinate_System:
         self.steps_per_rev = 360.0/self.stepper_degrees_per_step
         self.linear_distance_per_step = (np.pi * self.pulley_diameter)/self.steps_per_rev
 
-    def translate_position_to_stepper_commands(self, detected_balls):
-        for ball in detected_balls:
-            if ball["color"] == "white":
-                x,y = ball["position"]
-                x_distance_mm = x * self.width_mm_per_pixel
+    def translate_position_to_stepper_commands(self, detected_balls, labels):
+        boxes = detected_balls[0].boxes
+        for ball in boxes:
+            xyxy_tensor = ball.xyxy.cpu() # Detections in Tensor format in CPU memory
+            xyxy = xyxy_tensor.numpy().squeeze() # Convert tensors to Numpy array
+            xmin, ymin, xmax, ymax = xyxy.astype(int) # Extract individual coordinates and convert to int
+            classidx = int(ball.cls.item())
+            classname = labels[classidx]
+            middlex = (xmin + xmax) / 2
+            middley = (ymin + ymax) / 2
+            if classname == "white":
+                x_distance_mm = middlex * self.width_mm_per_pixel
                 x_step = x_distance_mm / self.linear_distance_per_step
-                y_distance_mm = y * self.height_mm_per_pixel
+                y_distance_mm = middley * self.height_mm_per_pixel
                 y_step = y_distance_mm / self.linear_distance_per_step
 
                 return (x_step, y_step)
